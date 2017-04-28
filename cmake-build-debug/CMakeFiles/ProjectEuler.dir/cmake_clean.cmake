5dab52776afe369bcd896d826dc2ff27087c8bd2
sunrise <tim_melnikov@mail.ru>
2017-04-28 20:23:23 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ProjectEuler.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectEuler.dir/main.c.o"
+  "ProjectEuler.pdb"
+  "ProjectEuler"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ProjectEuler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
