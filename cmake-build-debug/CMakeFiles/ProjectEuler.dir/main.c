57bbba7081df63f44f65fe07143295e100b6f9ac
sunrise <tim_melnikov@mail.ru>
2017-04-28 20:10:01 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ProjectEuler.dir/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/*ProjectEuler number 10*/
+/*Finding the sum of all the primes below two million.*/
+
+#define MAXNUM 2000000
+
+int primecheck(int num);
+
+int main(){
+    long sum = 0;  /* 2 is the lowest prime, so starting summarising from it*/
+
+    for(int i = 2;i < MAXNUM;i++){
+        int flag = primecheck(i);
+        if(flag == 1){
+            sum += i;
+        }
+    }
+
+    printf("%li/n",sum);
+    return 0;
+}
+
+int primecheck(int num){
+    int counter = 1;
+   /*if number is dividing only on 1 and itself, then it's a prime one*/
+    for(int i = 2;i < num;i++){
+        if(num % i == 0){
+            counter++;
+        }
+    }
+
+    return counter;
+}
