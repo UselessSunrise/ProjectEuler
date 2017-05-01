76f63dade536630306c29de2e7f240eda55b9105
sunrise <tim_melnikov@mail.ru>
2017-05-01 19:35:02 +0300

    New version for task 10.Using sieve of Eratosphen

--- task_010/main.c
+++ task_010/main.c
@@ -1,33 +1,42 @@
-#include <stdio.h>
+
 /*ProjectEuler number 10*/
 /*Finding the sum of all the primes below two million.*/
+/*In this version i'm using Sieve of Eratosphen to find prime numbers*/
 
 #define MAXNUM 2000000
 
-int primecheck(int num);
+int* sieve(int num[]);
 
 int main(){
-    long sum = 0;  /* 2 is the lowest prime, so starting summarising from it*/
-
-    for(int i = 2;i < MAXNUM;i++){
-        int flag = primecheck(i);
-        if(flag == 1){
-            sum += i;
+    /*It will be easier to deal with using massive*/
+    int numbers[MAXNUM];
+    int sum = 0;
+    /*Now we need to fill the massive with ones*/
+    for(int i = 0;i <= MAXNUM;i++){
+        numbers[i] = 1;
+    }
+    /*We're ready to use sieve*/
+    sieve(numbers);
+    /*Now we can summarise our primes*/
+    for(int i = 0;i <= MAXNUM;i++){
+        if(numbers[i] == 1){
+            sum+=(i+1);
+            /*adding i+1 because indexes are 0,1,2,etc.*/
         }
     }
-
-    printf("%li/n",sum);
     return 0;
 }
 
-int primecheck(int num){
-    int counter = 1;
-    /*if number is dividing only on 1 and itself, then it's a prime one*/
-    for(int i = 2;i < num;i++){
-        if(num % i == 0){
-            counter++;
+int* sieve(int num[]){
+    num[0] = 0; /*1 isn't a prime*/
+
+    for(int i = 1;i*i < MAXNUM;i++){
+        if(num[i] == 1){
+            for(int k = i * i;k <= MAXNUM;k += i){
+                num[i] = 0;
+            }
         }
     }
 
-    return counter;
+    return num;
 }
