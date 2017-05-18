a543af52ba8371279737910cf14303a1cf746ca3
sunrise <tim_melnikov@mail.ru>
2017-05-18 08:28:17 +0300

    Made program without dividing on functions, but with corrected sieve algorythm.

--- task_010/main.c
+++ task_010/main.c
@@ -1,22 +1,27 @@
-
+#include <stdio.h>
 /*ProjectEuler number 10*/
 /*Finding the sum of all the primes below two million.*/
 /*In this version i'm using Sieve of Eratosphen to find prime numbers*/
 
 #define MAXNUM 2000000
 
-int* sieve(int num[]);
-
 int main(){
-    /*It will be easier to deal with using massive*/
-    int numbers[MAXNUM];
-    int sum = 0;
+    long sum = 0;
+    int numbers[MAXNUM]; /*It will be easier to deal with using massive*/
     /*Now we need to fill the massive with ones*/
-    for(int i = 0;i <= MAXNUM;i++){
+    for(int i = 0;i <= MAXNUM;i++) {
         numbers[i] = 1;
     }
-    /*We're ready to use sieve*/
-    sieve(numbers);
+    numbers[0] = 0; /*1 isn't a prime*/
+
+    for(int i = 1;i*i < MAXNUM;i++){
+        if(numbers[i] == 1){
+            for(int k = (i+1) *(i+1);k <= MAXNUM;k += i+1 ){/*Adding one because indexeses are 1,2,3... but numbers are 2,3,4...*/
+                numbers[k-1] = 0;
+            }
+        }
+    }
+
     /*Now we can summarise our primes*/
     for(int i = 0;i <= MAXNUM;i++){
         if(numbers[i] == 1){
@@ -24,19 +29,8 @@ int main(){
             /*adding i+1 because indexes are 0,1,2,etc.*/
         }
     }
+
+    printf("%li",sum);
     return 0;
 }
 
-int* sieve(int num[]){
-    num[0] = 0; /*1 isn't a prime*/
-
-    for(int i = 1;i*i < MAXNUM;i++){
-        if(num[i] == 1){
-            for(int k = i * i;k <= MAXNUM;k += i){
-                num[i] = 0;
-            }
-        }
-    }
-
-    return num;
-}
