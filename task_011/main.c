34f000069a4a4bf41e93b0a86b9f3ea74dcb5e82
sunrise <tim_melnikov@mail.ru>
2017-06-14 14:46:05 +0300

    Fixed a lot of problems and mistakes in code. Also small comment fixes.

--- task_011/main.c
+++ task_011/main.c
@@ -1,29 +1,30 @@
 /*ProjectEuler number 11*/
 /*What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?*/
 #include <stdlib.h>
+#include <stdio.h>
 #define SIZE 19
 
 /* Calculate up,down,left,right and diagonally products to compare them later*/
-int leftprod(int grid[]);
-int rightprod(int grid[]);
-int upprod(int grid[]);
-int downprod(int grid[]);
-int ldiagprod(int grid[]);
-int rdiagprod(int grid[]);
+int leftprod(int grid[][SIZE]);
+int rightprod(int grid[][SIZE]);
+int upprod(int grid[][SIZE]);
+int downprod(int grid[][SIZE]);
+int ldiagprod(int grid[][SIZE]);
+int rdiagprod(int grid[][SIZE]);
 
 int main(){
-    /* Two-dimensional massive is going to be the grid*/
-    int grid[SIZE, SIZE];
+    /* Two-dimensional array is going to be the grid*/
+    int grid[SIZE][SIZE];
 
     /*The grid is filled with numbers from 0 to 99(randomly)*/
     for(int i = 0;i <= SIZE;i++){
         for(int j = 0;j <= SIZE;j++){
-            grid[i,j] = rand()%100;
+            grid[i][j] = rand()%100;
         }
     }
 
     int max =0;
-    int products[6];/*now we need to compare all the products, so I decided to use massive for that*/
+    int products[6];/*now we need to compare all the products, so I decided to use array for that*/
     for(int i = 0;i <= 5;i++){
         switch ( i ){
             case 0: products[i] = leftprod(grid);
@@ -32,6 +33,7 @@ int main(){
             case 3: products[i] = downprod(grid);
             case 4: products[i] = ldiagprod(grid);
             case 5: products[i] = rdiagprod(grid);
+            default:break;
         }
         if(products[i]>max){
             max = products[i];
@@ -43,72 +45,73 @@ int main(){
     return 0;
 }
 
-int leftprod(int grid[]){
+
+int leftprod(int grid[][SIZE]) {
     int max = 0;
     for(int i = 3;i <= SIZE;i++){
         for(int j = 0;j <= SIZE;j++){
-            if((grid[i,j]*grid[i-1,j]*grid[i-2,j]*grid[i-3]) > max){
-                max = grid[i,j]*grid[i-1,j]*grid[i-2,j]*grid[i-3];
+            if((grid[i][j] * grid[i-1][j] * grid[i-2][j] * grid[i-3][j]) > max){
+                max = grid[i][j]*grid[i-1][j]*grid[i-2][j]*grid[i-3][j];
             }
         }
     }
     return max;
 }
 
-int rightprod(int grid[]) {
+int rightprod(int grid[][SIZE]) {
     int max = 0;
     for (int i = 0; i <= SIZE - 3; i++) {
         for (int j = 0; j <= SIZE; j++) {
-            if ((grid[i, j] * grid[i + 1, j] * grid[i + 2, j] * grid[i + 3,j]) > max) {
-                max = grid[i, j] * grid[i + 1, j] * grid[i + 2, j] * grid[i + 3];
+            if ((grid[i][j] * grid[i + 1][j] * grid[i + 2][j] * grid[i + 3][j]) > max) {
+                max = grid[i][j] * grid[i + 1][j] * grid[i + 2][j] * grid[i + 3][j];
             }
         }
     }
     return max;
 }
 
-int upprod(int grid[]) {
+int upprod(int grid[][SIZE]) {
     int max = 0;
     for (int i = 0; i <= SIZE; i++) {
         for (int j = 3; j <= SIZE; j++) {
-            if ((grid[i, j-3] * grid[i, j-2] * grid[i, j-1] * grid[i,j]) > max) {
-                max = grid[i, j-3] * grid[i, j-2] * grid[i, j-1] * grid[i,j];
+            if ((grid[i][j-3] * grid[i][j-2] * grid[i][j-1] * grid[i][j]) > max) {
+                max = grid[i][j-3] * grid[i][j-2] * grid[i][j-1] * grid[i][j];
             }
         }
     }
     return max;
 }
 
-int downprod(int grid[]) {
+int downprod(int grid[][SIZE]) {
     int max = 0;
     for (int i = 0; i <= SIZE; i++) {
         for (int j = 0; j <= SIZE-3; j++) {
-            if ((grid[i, j+3] * grid[i, j+2] * grid[i, j+1] * grid[i,j]) > max) {
-                max = grid[i, j+3] * grid[i, j+2] * grid[i, j+1] * grid[i,j];
+            if ((grid[i][j+3] * grid[i][j+2] * grid[i][j+1] * grid[i][j]) > max) {
+                max = grid[i][j+3] * grid[i][j+2] * grid[i][j+1] * grid[i][j];
             }
         }
     }
     return max;
 }
 
-int ldiagprod(int grid[]) {
+int ldiagprod(int grid[][SIZE]) {
     int max = 0;
     for (int i = 3; i <= SIZE; i++) {
         for (int j = 0; j <= SIZE-3; j++) {
-            if ((grid[i-1, j+1] * grid[i-2, j+2] * grid[i-3, j+3] * grid[i,j]) > max) {
-                max = grid[i-1, j+1] * grid[i-2, j+2] * grid[i-3, j+3] * grid[i,j];
+            if ((grid[i-1][j+1] * grid[i-2][j+2] * grid[i-3][j+3] * grid[i][j]) > max) {
+                max = grid[i-1][j+1] * grid[i-2][j+2] * grid[i-3][j+3] * grid[i][j];
             }
         }
     }
     return max;
 }
 
-int rdiagprod(int grid[]) {
+int rdiagprod(int grid[][SIZE]) {
     int max = 0;
     for (int i = 0; i <= SIZE-3; i++) {
         for (int j = 0; j <= SIZE-3; j++) {
-            if ((grid[i+1, j+1] * grid[i+2, j+2] * grid[i+3, j+3] * grid[i,j]) > max) {
-                max = grid[i+1, j+1] * grid[i+2, j+2] * grid[i+3, j+3] * grid[i,j];
+            if ((grid[i+1][j+1] * grid[i+2][j+2] * grid[i+3][j+3] * grid[i][j]) > max) {
+                max = grid[i+1][j+1] * grid[i+2][j+2] * grid[i+3][j+3] * grid[i][j];
             }
         }
     }
